64551a307742580ef5f222eda14c834de8a5b571
Lasith Randima <lasithrandima123@gmail.com>
2024-06-12 13:47:28 +0530

    implimenting json server and fetch data & use spinners

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,26 +1,54 @@
 import React from 'react';
-import jobs from '../jobs.json';
+import { useEffect, useState } from 'react';
+// import jobs from '../jobs.json';
 import JobListing from '../components/JobListing.jsx';
+import Spinner from '../components/Spinner.jsx';
 
-const JobListings = () => {
+const JobListings = ({isHome = false}) => {
     // console.log(jobs);
-    const recentJobs = jobs.slice(0, 3)
+    // const jobListings = isHome ? jobs.slice(0, 3) : jobs;     // using jobs.json without server side
+
+    const [jobListings, setJobListings] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchJobs = async () => {
+          // limiting the number of jobs to 3 if it's the home page
+          const apiUrl = isHome ? 'http://localhost:3008/jobs?_limit=3' : 'http://localhost:3008/jobs';
+          try {
+            const response = await fetch(apiUrl);
+            const data = await response.json();
+            setJobListings(data);
+          } catch (error) {
+            console.log('Error Fetching Data', error);
+          } finally {
+            setLoading(false);
+          }
+           
+        }
+
+        fetchJobs();
+    }, [])
+
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-          Browse Jobs
+          {isHome ? 'Recent Jobs' : 'Browse Jobs'}
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        
 
-          {/* <!-- Job Listing 1 --> */}
+          {loading ? (<spinner loading={loading} />) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
 
-          {recentJobs.map((job) => (
-           <JobListing key={job.id} job={job} />
-          ))}
+          {jobListings.map((job) => (
+                    <JobListing key={job.id} job={job} />
+                    ))}
+          </div>
+          )}
+          
 
           
-        </div>
       </div>
     </section>
   )
